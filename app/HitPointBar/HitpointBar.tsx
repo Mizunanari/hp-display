4d6b0b0590ff091e672c54224fc5480ed36a9211
Mizunanari <Mizunanari@users.noreply.github.com>
2023-12-28 23:56:22 +0000

    HPの残量に合わせてHPバーの色を変更する

--- app/HitPointBar/HitpointBar.tsx
+++ app/HitPointBar/HitpointBar.tsx
@@ -4,9 +4,17 @@ import "./styles.css";
 
 export default function HealthBar({ maxHp = 100, hp = 100 } = {}) {
   const barWidth = (hp / maxHp) * 100;
+  let barColor = 'green';
+  if (barWidth >= 50) {
+    barColor = 'green';
+  } else if (barWidth >= 10) {
+    barColor = 'orange';
+  } else {
+    barColor = 'red';
+  }
   return (
     <div className="health-bar">
-      <div className="bar" style={{ width: `${barWidth}%` }}></div>
+      <div className="bar" style={{ width: `${barWidth}%`, backgroundColor: `${barColor}` }}></div>
       <div className="hit" style={{ width: `${0}%` }}></div>
       <div
         style={{
